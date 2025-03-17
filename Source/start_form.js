d677f649eb1b8a58ea4b19b450722b9022519fdd
Paul-Fournet <144245169+Paul-Fournet@users.noreply.github.com>
2025-03-17 09:56:05 +0100

    aa

--- Source/start_form.js
+++ Source/start_form.js
@@ -2,17 +2,21 @@
 let zone_jeu=document.getElementById("zone_jeu");
 
 let form_level=document.getElementById("level_choice");
+
 let choice_size=document.getElementById("choice_grid_size");
+
 let choice_timer_yes=document.getElementById("timer_yes");
 let choice_timer_no=document.getElementById("timer_no");
+
 let div_time_cursor=document.getElementById("div_time_cursor");
+let time_cursor=document.getElementsById("time_cursor");
 
 
 form_level.addEventListener("submit", function(event){
     event.preventDefault();
-    console.log("formulaire envoyé");
-
+    console.log(choice_size.value);
     let val_choice=choice_size.value;  
+    console.log(val_choice);
 })
 
 
@@ -28,15 +32,19 @@ choice_timer_no.addEventListener("click", function(){
 
 function refresh_time_cursor(curs_state){
     if(curs_state){
-        div_time_cursor.innerHTML=`<label for="game_length">Length (seconds):<br> 60</label>
-                                   <input type="range" id="time_cursor" name="time" min="60" max="600">600`
+        div_time_cursor.style.display="inline-block";
     }
     else{
-        div_time_cursor.innerHTML=``;
+        div_time_cursor.style.display="none";
     }
 }
 
-let time_cursor=document.getElementsById("time_cursor");
+
+
+
+console.log(time_cursor)
+console.log("bonjour")
+
 time_cursor.addEventListener("input",function(){
     console.log(time_cursor.value);
 })
