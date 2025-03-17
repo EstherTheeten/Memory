1974978192a88f04c9d7063e53b6d302891c3f75
Paul-Fournet <144245169+Paul-Fournet@users.noreply.github.com>
2025-03-17 09:56:18 +0100

    aa

--- Source/start_form.js
+++ Source/start_form.js
@@ -9,26 +9,32 @@ let choice_timer_yes=document.getElementById("timer_yes");
 let choice_timer_no=document.getElementById("timer_no");
 
 let div_time_cursor=document.getElementById("div_time_cursor");
-let time_cursor=document.getElementsById("time_cursor");
+let time_cursor=document.getElementById("time_cursor");
 
+let error_message=document.querySelector(".error_message");
 
 form_level.addEventListener("submit", function(event){
     event.preventDefault();
-    console.log(choice_size.value);
-    let val_choice=choice_size.value;  
-    console.log(val_choice);
+    
+    let res=choice_size.value;
+
+    if(res==="none"){
+        error_message.style.display="inline-block";
+    }
+    else{
+        error_message.style.display="none";
+    }
+
+    if(choice_timer_yes.checked){
+        res+=" "+time_cursor.value;
+    }
+    else{
+        res+=" "+"no_timer";
+    }
+    console.log(res)
+    
 })
 
-
-
-
-choice_timer_yes.addEventListener("click", function(){
-    refresh_time_cursor(true)
-})
-
-choice_timer_no.addEventListener("click", function(){
-    refresh_time_cursor(false)
-})
 
 function refresh_time_cursor(curs_state){
     if(curs_state){
@@ -38,6 +44,14 @@ function refresh_time_cursor(curs_state){
         div_time_cursor.style.display="none";
     }
 }
+
+choice_timer_yes.addEventListener("click", function(){
+    refresh_time_cursor(true)
+})
+choice_timer_no.addEventListener("click", function(){
+    refresh_time_cursor(false)
+})
+
 
 
 
